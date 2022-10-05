47498654c4c75d02e759650e4f850480bd804390
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-10-05 14:29:46 +0000

    add: basic tests

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hola asdasd")
+import { Observable, Observer } from "rxjs";
+const observableAlpha$ = new Observable<number|string>((subscriber) => {
+  subscriber.next("Alpha");
+  subscriber.next("Beta");
+  subscriber.next(1);
+  subscriber.next("Gamma");
+  subscriber.complete();
+});
+
+const observer:Observer<number | string>  = {
+  next: (value:number | string) => value,
+  error: (error:any) => console.log(error),
+  complete: () => console.log("Completed")
+}
+
+observableAlpha$.subscribe(observer);
+
