ed960cf762a6c5d1e45e1fc6909498560f0a9001
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-10-05 18:34:27 +0000

    add: track user attempt and answers

--- src/index.ts
+++ src/index.ts
@@ -6,28 +6,33 @@ const onKeyDown$: Observable<KeyboardEvent> = fromEvent<KeyboardEvent>(
 );
 let letterIndex = 0;
 let letterRowIndex = 0;
+let userAnswers: string[] = [];
+let userAttempt: string[] = [];
 const insertLetter: Observer<KeyboardEvent> = {
   next: (event: KeyboardEvent) => {
     const pressedKey = event.key.toUpperCase();
-   
+
     if (pressedKey.length === 1 && pressedKey.match(/[a-z]/i)) {
       let letterBox =
-      Array.from(letterRows)[letterRowIndex].children[letterIndex];
+        Array.from(letterRows)[letterRowIndex].children[letterIndex];
       letterBox.textContent = pressedKey;
       letterBox.classList.add("filled-letter");
+      userAttempt.push(pressedKey);
       if (letterIndex === 4) {
         letterIndex = 0;
         letterRowIndex++;
+        userAnswers.push(userAttempt.join(""));
+        userAttempt = [];
       } else {
         letterIndex++;
       }
-    }
-    else if (pressedKey === 'BACKSPACE'){
-      letterIndex= letterIndex - 1 >= 0 ? letterIndex-1 : 0;
+    } else if (pressedKey === "BACKSPACE") {
+      letterIndex = letterIndex - 1 >= 0 ? letterIndex - 1 : 0;
       let letterBox =
-      Array.from(letterRows)[letterRowIndex].children[letterIndex];
+        Array.from(letterRows)[letterRowIndex].children[letterIndex];
       letterBox.classList.remove("filled-letter");
-      letterBox.textContent = '';
+      letterBox.textContent = "";
+      userAttempt.pop();
     }
   },
   error: (error: Error) => {
@@ -35,5 +40,4 @@ const insertLetter: Observer<KeyboardEvent> = {
   },
   complete: () => {},
 };
-
 onKeyDown$.subscribe(insertLetter);
