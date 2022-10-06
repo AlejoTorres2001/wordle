580b68e736ffe3fe05b7c7f0a4ed9a2aa44542c4
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-10-06 10:25:48 -0300

    add: routing config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: { main:'./src/index.ts',board:'./src/board.ts' },
   devtool: 'inline-source-map',
   mode: 'development',
   module: {
@@ -17,7 +18,21 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+        chunkFilename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+      chunks: ['main']
+
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/board.html',
+      filename: 'board.html',
+      chunks: ['board']
+    })
+  ]
 };
