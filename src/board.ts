51a7af6eea4109b9b2984b19f6756d6b06fa702f
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-10-06 17:25:14 -0300

    add: offset on canvas position

--- src/board.ts
+++ src/board.ts
@@ -1,15 +1,23 @@
 import { fromEvent, map } from "rxjs";
 
-const canvas = document.getElementById('id-reactive-canvas') as HTMLCanvasElement;
-const onMouseOver$ = fromEvent<MouseEvent>(canvas, 'mousemove').pipe(
-  map((event:MouseEvent) => {
+const canvas = document.getElementById(
+  "id-reactive-canvas"
+) as HTMLCanvasElement;
+const onMouseOver$ = fromEvent<MouseEvent>(canvas, "mousemove").pipe(
+  map((event: MouseEvent) => {
     return {
       x: event.x,
-      y: event.y
-    }
+      y: event.y,
+    };
+  }),
+  map((position) => {
+    return {
+      x: position.x - canvas.offsetLeft,
+      y: position.y - canvas.offsetTop,
+    };
   })
-)
-const onMouseDown$ = fromEvent<MouseEvent>(canvas, 'mousedown')
-const onMouseUp$ = fromEvent<MouseEvent>(canvas, 'mouseup')
+);
+const onMouseDown$ = fromEvent<MouseEvent>(canvas, "mousedown");
+const onMouseUp$ = fromEvent<MouseEvent>(canvas, "mouseup");
 
-onMouseOver$.subscribe(console.log)
+onMouseOver$.subscribe(console.log);
