d421520a75395b7fd5b5d1b85c6d44dc9dea89d5
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-10-06 02:26:10 +0000

    refactor: using services

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,55 @@
-import { fromEvent, Observable, Observer } from "rxjs";
-const letterRows = document.getElementsByClassName("letter-row");
-const onKeyDown$: Observable<KeyboardEvent> = fromEvent<KeyboardEvent>(
-  document,
-  "keydown"
-);
+import { fromEvent, Observable, Observer, Subject } from "rxjs";
+import { onCompleteData } from "./@types";
+import { checkLetters } from "./services/checkLetters";
+import { getRandomWord } from "./services/getRandomWord";
+import { isLetter } from "./services/isLetter";
+import { isWordCorrect } from "./services/isWordCorrect";
+import { setMessage } from "./services/setMessage";
+
 let letterIndex = 0;
 let letterRowIndex = 0;
 let userAnswers: string[] = [];
 let userAttempt: string[] = [];
+let rightWord = getRandomWord();
+const letterRows = document.getElementsByClassName("letter-row");
+const restartButton = document.getElementById("restart-button");
+const onKeyDown$: Observable<KeyboardEvent> = fromEvent<KeyboardEvent>(
+  document,
+  "keydown"
+);
+const onRowCompleted$: Subject<onCompleteData> = new Subject();
+
 const insertLetter: Observer<KeyboardEvent> = {
   next: (event: KeyboardEvent) => {
     const pressedKey = event.key.toUpperCase();
 
-    if (pressedKey.length === 1 && pressedKey.match(/[a-z]/i)) {
+    if (isLetter(pressedKey)) {
       let letterBox =
         Array.from(letterRows)[letterRowIndex].children[letterIndex];
       letterBox.textContent = pressedKey;
       letterBox.classList.add("filled-letter");
       userAttempt.push(pressedKey);
       if (letterIndex === 4) {
+        const userAnswer = userAttempt.join("");
+        userAnswers.push(userAnswer);
+        onRowCompleted$.next({ userAnswer, letterRowIndex });
+        userAttempt = [];
         letterIndex = 0;
         letterRowIndex++;
-        userAnswers.push(userAttempt.join(""));
-        userAttempt = [];
-      } else {
-        letterIndex++;
+        return;
       }
-    } else if (pressedKey === "BACKSPACE") {
+      letterIndex++;
+    }
+  },
+  error: (error: Error) => {
+    console.error(error);
+  },
+  complete: () => {},
+};
+const deleteLetter: Observer<KeyboardEvent> = {
+  next: (event: KeyboardEvent) => {
+    const pressedKey = event.key.toUpperCase();
+    if (pressedKey === "BACKSPACE") {
       letterIndex = letterIndex - 1 >= 0 ? letterIndex - 1 : 0;
       let letterBox =
         Array.from(letterRows)[letterRowIndex].children[letterIndex];
@@ -35,9 +58,23 @@ const insertLetter: Observer<KeyboardEvent> = {
       userAttempt.pop();
     }
   },
+  complete: () => {},
   error: (error: Error) => {
     console.error(error);
   },
-  complete: () => {},
 };
 onKeyDown$.subscribe(insertLetter);
+onKeyDown$.subscribe(deleteLetter);
+onRowCompleted$.subscribe((data: onCompleteData) => {
+  const { userAnswer, letterRowIndex } = data;
+  console.log(userAnswer, rightWord, letterIndex);
+  const letters = [...Array.from(letterRows[letterRowIndex].children)];
+  checkLetters(letters, rightWord);
+  if (!isWordCorrect(userAnswer, rightWord) && letterRowIndex !== 5) return;
+  if (isWordCorrect(userAnswer, rightWord)) {
+    setMessage("success", "Correct!");
+  } else if (letterRowIndex === 5) {
+    setMessage("error", `Game over! The word was ${rightWord}`);
+  }
+  restartButton.removeAttribute("disabled");
+});
