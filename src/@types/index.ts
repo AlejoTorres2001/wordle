f407a8dc4ae6ce90321db3734cb9127923030edc
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-10-07 04:48:07 +0000

    add: types

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -2,3 +2,14 @@ export type onCompleteData = {
   userAnswer: string;
   letterRowIndex: number;
 };
+export type Position = {
+  x: number;
+  y: number;
+}
+export type ContextOptions = {
+  lineWidth?: number;
+  strokeStyle?: string;
+  lineJoin?: CanvasLineJoin;
+  lineCap?: CanvasLineCap;
+
+}
