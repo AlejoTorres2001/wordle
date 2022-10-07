c3a099b9c1f2106a1884d65a422b76922ff83470
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-10-07 04:48:39 +0000

    add: white board

--- src/board.ts
+++ src/board.ts
@@ -1,23 +1,36 @@
-import { fromEvent, map } from "rxjs";
+import { fromEvent, map, mergeAll, takeUntil } from "rxjs";
+import { Position } from "./@types";
+import { configureContext, updateCursorPosition } from "./services/canvas";
 
 const canvas = document.getElementById(
   "id-reactive-canvas"
 ) as HTMLCanvasElement;
+
+const cursorPosition: Position = { x: 0, y: 0 };
+const offSet = {
+  offsetLeft: canvas.offsetLeft,
+  offsetTop: canvas.offsetTop,
+};
+const paintStroke = (event: MouseEvent) => {
+  const ctx = configureContext(canvas, {});
+  ctx.beginPath();
+  ctx.moveTo(cursorPosition.x, cursorPosition.y);
+  updateCursorPosition(cursorPosition, offSet, event);
+  ctx.lineTo(cursorPosition.x, cursorPosition.y);
+  ctx.stroke();
+  ctx.closePath();
+};
+const onMouseUp$ = fromEvent<MouseEvent>(canvas, "mouseup");
 const onMouseOver$ = fromEvent<MouseEvent>(canvas, "mousemove").pipe(
-  map((event: MouseEvent) => {
-    return {
-      x: event.x,
-      y: event.y,
-    };
-  }),
-  map((position) => {
-    return {
-      x: position.x - canvas.offsetLeft,
-      y: position.y - canvas.offsetTop,
-    };
-  })
+  takeUntil(onMouseUp$)
 );
 const onMouseDown$ = fromEvent<MouseEvent>(canvas, "mousedown");
-const onMouseUp$ = fromEvent<MouseEvent>(canvas, "mouseup");
 
-onMouseOver$.subscribe(console.log);
+const startPaint$ = onMouseDown$.pipe(
+  map(() => onMouseOver$),
+  mergeAll()
+);
+onMouseDown$.subscribe((event: MouseEvent) =>
+  updateCursorPosition(cursorPosition, offSet, event)
+);
+startPaint$.subscribe(paintStroke);
