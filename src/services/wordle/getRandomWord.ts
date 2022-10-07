66b95414f59f402880cf53f5726fc2d0fe17c347
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-10-07 04:47:42 +0000

    refactored: changed service folder structure

--- src/services/wordle/getRandomWord.ts
+++ src/services/wordle/getRandomWord.ts
@@ -1,3 +1,3 @@
-import WORDS_LIST from "../wordsList.json";
+import WORDS_LIST from "../../wordsList.json";
 export const getRandomWord = () =>
   WORDS_LIST[Math.floor(Math.random() * WORDS_LIST.length)];
